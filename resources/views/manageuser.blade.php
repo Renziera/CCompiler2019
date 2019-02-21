5a80e24204ae741f52e75ecc55dd818b127d3262
verm024 <verm024>
2019-02-21 23:33:12 +0700

    dashboard admin, manage user, view member admin

--- resources/views/manageuser.blade.php
+++ resources/views/manageuser.blade.php
@@ -1,56 +1,137 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Manage User</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    <table width="100%">
-                        <tr>
-                            <th>Nama</th>
-                            <th>Username</th>
-                            <th>Role</th>
-                            <th>Status</th>
-                            <th>Action</th>
-                            <th>Upgrade</th>
-                        </tr>
-                        @foreach($data as $user)
-                            <tr>
-                                <td>{{$user['name']}}</td>
-                                <td>{{$user['username']}}</td>
-                                <td>{{$user['role']}}</td>
-                                <td>{{$user['status']}}</td>
-                                <td>
-                                    @if($user['status'] == 'OK')
-                                        <form action="/admin/viewmembers" method="post">
-                                            @csrf
-                                            <input type="hidden" name="username" value="{{$user['username']}}">
-                                            <input type="submit" value="Members">
-                                        </form>
-                                    @else
-                                        none
-                                    @endif
-                                </td>
-                                <td>
-                                    @if($user['status'] == 'OK')
-                                        <form action="/admin/approve" method="post">
-                                            @csrf
-                                            <input type="hidden" name="username" value="{{$user['username']}}">
-                                            <input type="submit" value="Upgrade">
-                                        </form>
-                                    @elseif($user['status'] == 'Approved')
-                                        Upgraded
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                    </table>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'CCompiler') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/css/viewmemb.css">
+</head>
+
+<body>
+    <div class="bg-viewmemb">
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light nav-css navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="/image/ccomp%20hd-white.png" alt="" width="100px">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}" style="color: #F9FBFC;">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}" style="color: #F9FBFC;">{{ __('Register') }}</a>
+                            </li>
+                            @endif
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">Manage User</div>
+
+                                <div class="card-body">
+                                    <table width="100%">
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Username</th>
+                                            <th>Role</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                            <th>Upgrade</th>
+                                        </tr>
+                                        @foreach($data as $user)
+                                        <tr>
+                                            <td>{{$user['name']}}</td>
+                                            <td>{{$user['username']}}</td>
+                                            <td>{{$user['role']}}</td>
+                                            <td>{{$user['status']}}</td>
+                                            <td>
+                                                @if($user['status'] == 'OK')
+                                                <form action="/admin/viewmembers" method="post">
+                                                    @csrf
+                                                    <input type="hidden" name="username" value="{{$user['username']}}">
+                                                    <input type="submit" value="Members">
+                                                </form>
+                                                @else
+                                                none
+                                                @endif
+                                            </td>
+                                            <td>
+                                                @if($user['status'] == 'OK')
+                                                <form action="/admin/approve" method="post">
+                                                    @csrf
+                                                    <input type="hidden" name="username" value="{{$user['username']}}">
+                                                    <input type="submit" value="Upgrade">
+                                                </form>
+                                                @elseif($user['status'] == 'Approved')
+                                                Upgraded
+                                                @endif
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
