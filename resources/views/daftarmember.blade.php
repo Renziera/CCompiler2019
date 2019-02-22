c55ba274348814d52773b351819754de7bba996b
verm024 <verm024>
2019-02-22 21:28:44 +0700

    daftar member+ upload proposal

--- resources/views/daftarmember.blade.php
+++ resources/views/daftarmember.blade.php
@@ -1,111 +1,191 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Data anggota</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                    <form method="POST" action="/peserta/member">
-                        @csrf
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group row">
-                            <label for="member" class="col-md-4 col-form-label text-md-right">{{ __('Member 1') }}</label>
-                        </div>
+    <title>{{ config('app.name', 'CCompiler') }}</title>
 
-                        <div class="form-group row">
-                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                            <div class="col-md-6">
-                                <input id="nama" type="text" class="form-control" name="nama1" required autofocus>
-                            </div>
-                        </div>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
-                        <div class="form-group row">
-                            <label for="nim" class="col-md-4 col-form-label text-md-right">{{ __('NIM') }}</label>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/css/cabang.css">
+</head>
 
-                            <div class="col-md-6">
-                                <input id="nim" type="text" class="form-control" name="nim1" required>
-                            </div>
-                        </div>
+<body>
+    <div class="bg-cabang">
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="/image/ccomp%20hd-white.png" alt="" width="100px">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                        <div class="form-group row">
-                            <label for="prodi" class="col-md-4 col-form-label text-md-right">{{ __('Prodi') }}</label>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                            <div class="col-md-6">
-                                <input id="prodi" type="text" class="form-control" name="prodi1" required>
-                            </div>
-                        </div>
+                        </ul>
 
-                        <div class="form-group row">
-                            <label for="member" class="col-md-4 col-form-label text-md-right">{{ __('Member 2') }}</label>
-                        </div>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                            @endif
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-                        <div class="form-group row">
-                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-                            <div class="col-md-6">
-                                <input id="nama" type="text" class="form-control" name="nama2" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="nim" class="col-md-4 col-form-label text-md-right">{{ __('NIM') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nim" type="text" class="form-control" name="nim2" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="prodi" class="col-md-4 col-form-label text-md-right">{{ __('Prodi') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="prodi" type="text" class="form-control" name="prodi2" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="member" class="col-md-4 col-form-label text-md-right">{{ __('Member 3') }}</label>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nama" type="text" class="form-control" name="nama3" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="nim" class="col-md-4 col-form-label text-md-right">{{ __('NIM') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="nim" type="text" class="form-control" name="nim3" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="prodi" class="col-md-4 col-form-label text-md-right">{{ __('Prodi') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="prodi" type="text" class="form-control" name="prodi3" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Submit') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                           <div class="card-header2">Yuk, Daftarin Anggotamu!</div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <form method="POST" action="/peserta/member">
+                                        @csrf
+
+                                        <div class="anggota">
+                                            <label for="member" class="label-anggota">{{ __('Anggota 1') }}</label>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="nama" type="text" class="form-control" name="nama1" required autofocus>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="nim" class="col-md-4 col-form-label text-md-right">{{ __('NIM') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="nim" type="text" class="form-control" name="nim1" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row last">
+                                            <label for="prodi" class="col-md-4 col-form-label text-md-right">{{ __('Prodi') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="prodi" type="text" class="form-control" name="prodi1" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="anggota">
+                                            <label for="member" class="label-anggota">{{ __('Anggota 2') }}</label>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="nama" type="text" class="form-control" name="nama2" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="nim" class="col-md-4 col-form-label text-md-right">{{ __('NIM') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="nim" type="text" class="form-control" name="nim2" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row last">
+                                            <label for="prodi" class="col-md-4 col-form-label text-md-right">{{ __('Prodi') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="prodi" type="text" class="form-control" name="prodi2" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="anggota">
+                                            <label for="member" class="label-anggota">{{ __('Anggota 3') }}</label>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="nama" type="text" class="form-control" name="nama3" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="nim" class="col-md-4 col-form-label text-md-right">{{ __('NIM') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="nim" type="text" class="form-control" name="nim3" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row last">
+                                            <label for="prodi" class="col-md-4 col-form-label text-md-right">{{ __('Prodi') }}</label>
+
+                                            <div class="col-md-6">
+                                                <input id="prodi" type="text" class="form-control" name="prodi3" required>
+                                            </div>
+                                        </div>
+
+                                        <div class="">
+                                            <div class="card-button">
+                                                <button type="submit" class="btn btn-primary">
+                                                    {{ __('Submit') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
--- resources/views/uploadproposal.blade.php
+++ resources/views/uploadproposal.blade.php
@@ -1,35 +1,120 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Upload proposal</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="card-body">
-                <form method="POST" action="/peserta/proposal" enctype="multipart/form-data">
-                        @csrf
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group row">
-                            <label for="cabang" class="col-md-4 col-form-label text-md-right">{{ __('Upload proposal anda') }}</label>
+    <title>{{ config('app.name', 'CCompiler') }}</title>
 
-                            <div class="col-md-6">
-                                <input type="file" name="proposal" id="proposal" accept=".pdf">
-                            </div>
-                        </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Upload') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="/css/cabang.css">
+</head>
+
+<body>
+    <div class="bg-cabang">
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="/image/ccomp%20hd-white.png" alt="" width="100px">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
+
+                        </ul>
+
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                            @endif
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                           <div class="card-header2">Kuy, Upload Proposal!</div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <form method="POST" action="/peserta/proposal" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="card-button">
+                                            <img src="/image/icon-upload.png" alt="" width="120px">
+                                        </div>
+                                        <div class="text-upload card-button">
+                                            Upload
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="cabang" class="col-md-4 col-form-label text-md-right" style="display: none;">{{ __('Upload proposal anda') }}</label>
+
+                                            <div class="upload">
+                                                <input type="file" name="proposal" id="proposal" accept=".pdf" class="input">
+                                            </div>
+                                        </div>
+
+                                        <div class="">
+                                            <div class="card-button">
+                                                <button type="submit" class="btn btn-primary">
+                                                    {{ __('Upload') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
