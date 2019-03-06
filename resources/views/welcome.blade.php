869b4e6be2cd8f482dbb5fa64b9af32d8675e94f
verm024 <verm024>
2019-03-06 16:24:02 +0700

    fix landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,9 @@
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
+    <!-- Script-->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
     <style>
         html,
         body {
@@ -51,12 +54,12 @@
             text-align: center;
             display: none;
         }
-        
-        .card{
+
+        .card {
             background-color: #F9FBFC;
             border: none;
         }
-        
+
         .links>a {
             margin-top: 20px;
             font-family: 'Montserrat';
@@ -89,6 +92,7 @@
 
 <body>
 
+    <!--
     <div class="flex-center position-ref">
         @if (Route::has('login'))
         <div class="top-right links">
@@ -104,9 +108,48 @@
         </div>
         @endif
     </div>
+-->
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="/image/ccomp%20hd-white.png" alt="" width="100px">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                @if (Route::has('login'))
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/home') }}" style="color: #F9FBFC;">Beranda</a>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}" style="color: #F9FBFC;">Login</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}" style="color: #F9FBFC;">Register</a>
+                    </li>
+                    @endif
+                    @endauth
+                </ul>
+                @endif
+            </div>
+        </div>
+    </nav>
+
+
+    <!--
     <div class="nav-logo">
         <a href="#"><img src="/image/ccomp%20hd-white.png" alt="" width="100px"></a>
     </div>
+-->
+
     <!--    header start-->
     <header>
         <div class="welcome">
@@ -136,7 +179,7 @@
         </div>
         <div class="card">
             <div class="card-lomba">
-                
+
                 <div class="card-img">
                     <img src="/image/icon-cp.png" alt="" width="90px">
                 </div>
